ef198987019d55a69f1cc1923ed79f82d5e47cf4
BrainBlasted <brainblastedmods@gmail.com>
2017-09-05 20:57:18 -0400

    Finished DivisibleCheck.java

--- src/brainblasted/math/DivisibleCheck.java
+++ src/brainblasted/math/DivisibleCheck.java
@@ -11,8 +11,21 @@ import java.util.*;
 
 public class DivisibleCheck
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner keyboard = new Scanner(System.in);
-        
+        System.out.print("Please enter your first number: ");
+        double firstNum  = keyboard.nextDouble();
+        System.out.print("Please enter your second number: ");
+        double secondNum  = keyboard.nextDouble();
+        keyboard.close();
+        if ((firstNum % secondNum) == 0)
+        {
+            System.out.printf("%.1f is divisible by %.1f. %n" +
+                            "The quotient is %.1f.", firstNum, secondNum, (firstNum/secondNum));
+        } else
+        {
+            System.out.printf("%.1f & %.1f are not divisible.", firstNum, secondNum);
+        }
     }
 }
