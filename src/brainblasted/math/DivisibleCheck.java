34cf8aa4442e4fa1e32b63e052a502cfcafb7411
BrainBlasted <brainblastedmods@gmail.com>
2017-09-05 20:41:53 -0400

    Added DivisibileCheck.java

---
+++ src/brainblasted/math/DivisibleCheck.java
@@ -0,0 +1,18 @@
+/**
+ * DivisibleCheck.java
+ *  Checks the whether 'a' is divisible by 'b'.
+ * 
+ * @author Christopher Davis
+ * @version 20170905
+ */
+package brainblasted.math;
+
+import java.util.*;
+
+public class DivisibleCheck
+{
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+    }
+}
